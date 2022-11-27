d447891b120a3f9c6960b090a8612be14710253e
Hajduk Dušan <62693918+HajdukDusan@users.noreply.github.com>
2022-11-27 11:23:39 +0100

    add cmd conf

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -3,36 +3,20 @@ package main
 import (
 	"backend/contracts/StupidContract"
 	"backend/internal/examples"
+	"backend/pkg/gef"
 	"log"
 	"os"
-	"time"
 
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/ethclient"
-	"github.com/joho/godotenv"
 )
 
 func main() {
 
-	// Load Env File
-	err := godotenv.Load()
-	if err != nil {
-		panic("Error loading .env file")
-	}
+	//###########################
+	client, privKey := gef.ConfigureClient(os.Args[1])
+	//###########################
 
-	// privKey := os.Getenv("MAINNET_PRIVATE_KEY")
-	// privKey := os.Getenv("GOERLI_PRIVATE_KEY")
-	privKey := os.Getenv("LOCALHOST_PRIVATE_KEY")
-
-	// rpcUrl := os.Getenv("ALCHEMY_MAINNET_RPC_WS_URL")
-	// rpcUrl := os.Getenv("ALCHEMY_GOERLI_RPC_WS_URL")
-	rpcUrl := os.Getenv("LOCALHOST_RPC_WS_URL")
-
-	client, err := ethclient.Dial(rpcUrl)
-	if err != nil {
-		panic(err)
-	}
-
+	//###########################
 	stupidContractAPI, err := StupidContract.NewStupidContract(
 		common.HexToAddress(StupidContract.Address),
 		client,
@@ -40,22 +24,11 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	//###########################
 
-	// usage examples
-
+	//###########################
 	go examples.SubscribeToEvent(client, stupidContractAPI)
-
-	time.Sleep(5 * time.Second)
-
 	// examples.SendNormalTx(client, privKey, "0x70997970C51812dc3A010C7d01b50e0d17dc79C8")
-	examples.SendContractTx(client, privKey, stupidContractAPI)
-
-	time.Sleep(5 * time.Second)
-
-	examples.SendContractTx(client, privKey, stupidContractAPI)
-
-	time.Sleep(10 * time.Second)
-
 	examples.SendContractTx(client, privKey, stupidContractAPI)
 	// examples.GetContractLogs(client, stupidContractAPI)
 	// examples.CallContractViewFunc(client, stupidContractAPI)
--- backend/pkg/gef/gef.go
+++ backend/pkg/gef/gef.go
@@ -2,16 +2,58 @@ package gef
 
 import (
 	"math/big"
+	"os"
+	"strings"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/joho/godotenv"
 )
 
 type EventWrapper struct {
 	Name        string
 	Args        []string
 	ParseMethod func(types.Log) (interface{}, error)
+}
+
+// Configures the client and the private key depending on the network.
+// If used, env folder should be configured
+//
+// Arguments:
+//   - connection - MAINNET, TESTNET or LOCALHOST. Defaults to LOCALHOST.
+func ConfigureClient(connection string) (*ethclient.Client, string) {
+
+	// Load Env File
+	err := godotenv.Load()
+	if err != nil {
+		panic("Error loading .env file")
+	}
+
+	var rpcUrl string
+	var privKey string
+
+	switch strings.ToUpper(connection) {
+	case "MAINNET":
+		privKey = os.Getenv("MAINNET_PRIVATE_KEY")
+		rpcUrl = os.Getenv("MAINNET_RPC_WS_URL")
+	case "TESTNET":
+		privKey = os.Getenv("TESTNET_PRIVATE_KEY")
+		rpcUrl = os.Getenv("TESTNET_RPC_WS_URL")
+	case "LOCALHOST":
+		privKey = os.Getenv("LOCALHOST_PRIVATE_KEY")
+		rpcUrl = os.Getenv("LOCALHOST_RPC_WS_URL")
+	default:
+		privKey = os.Getenv("LOCALHOST_PRIVATE_KEY")
+		rpcUrl = os.Getenv("LOCALHOST_RPC_WS_URL")
+	}
+
+	client, err := ethclient.Dial(rpcUrl)
+	if err != nil {
+		panic(err)
+	}
+
+	return client, privKey
 }
 
 // GetLogs returns logs for specified events within a certain block range.
